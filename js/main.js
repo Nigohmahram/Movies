e36861697c2afd4ac0b62d4fde900c47a22ca191
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-02-28 17:20:55 +0900

    input json-server

--- js/main.js
+++ js/main.js
@@ -305,4 +305,8 @@ window.addEventListener('DOMContentLoaded', () => {
                closeModal();
           }, 4000);
      }
+
+     fetch('db.json')
+          .then((data) => data.json())
+          .then((res) => console.log(res));
 });
